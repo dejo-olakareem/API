cfae4b5182ccb93c09310204f771d803039985b0
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-12-30 17:55:11 -0800

    Learn API and Json Parse

---
+++ app/controllers/index.rb
@@ -0,0 +1,27 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+get "/" do
+	erb :"index.html"
+end
+
+post "/learn" do
+url = URI("https://api.github.com/users/#{params[:github]}")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["cache-control"] = 'no-cache'
+request["postman-token"] = '7c05e845-d147-d76c-bc3d-97581694a023'
+
+response = http.request(request)
+puts response.read_body
+
+patch = JSON.parse(response.read_body)
+puts patch
+@answer = patch["name"]
+	erb :"index.html"
+end
