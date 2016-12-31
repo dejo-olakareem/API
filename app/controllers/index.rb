d621dd6cc39343af87337a6a29c96ec0ed180f1f
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-12-31 14:43:44 -0800

    Implement simple API call to generate github name

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,21 +7,23 @@ get "/" do
 end
 
 post "/learn" do
-url = URI("https://api.github.com/users/#{params[:github]}")
+		url = URI("https://api.github.com/users/#{params[:github]}")
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		http = Net::HTTP.new(url.host, url.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-request = Net::HTTP::Get.new(url)
-request["cache-control"] = 'no-cache'
-request["postman-token"] = '7c05e845-d147-d76c-bc3d-97581694a023'
+		request = Net::HTTP::Get.new(url)
+		request["cache-control"] = 'no-cache'
+		request["postman-token"] = '7c05e845-d147-d76c-bc3d-97581694a023'
 
-response = http.request(request)
-puts response.read_body
+		response = http.request(request)
+		puts response.read_body
 
-patch = JSON.parse(response.read_body)
-puts patch
-@answer = patch["name"]
-	erb :"index.html"
+		patch = JSON.parse(response.read_body)
+		puts patch
+		@answer = patch["name"]
+		erb :"index.html"
 end
+
+
