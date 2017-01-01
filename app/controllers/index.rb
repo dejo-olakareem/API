2955a7757bdd9d4d383eae4f0c900afffd3113b6
dejo-olakareem <oladejoolakareem@yahoo.com>
2016-12-31 16:52:49 -0800

    Edit API

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,23 +7,23 @@ get "/" do
 end
 
 post "/learn" do
-		url = URI("https://api.github.com/users/#{params[:github]}")
+	url = URI("https://api.github.com/users/#{params[:github]}")
 
-		http = Net::HTTP.new(url.host, url.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	http = Net::HTTP.new(url.host, url.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-		request = Net::HTTP::Get.new(url)
-		request["cache-control"] = 'no-cache'
-		request["postman-token"] = ENV["HIDE"]
+	request = Net::HTTP::Get.new(url)
+	request["cache-control"] = 'no-cache'
+	request["postman-token"] = ENV["HIDE"]
 
-		response = http.request(request)
-		puts response.read_body
+	response = http.request(request)
+	puts response.read_body
 
-		patch = JSON.parse(response.read_body)
-		puts patch
-		@answer = patch["name"]
-		erb :"index.html"
+	patch = JSON.parse(response.read_body)
+	puts patch
+	@answer = patch["name"]
+	erb :"index.html"
 end
 
 
